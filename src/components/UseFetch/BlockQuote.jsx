3bcac38805a15f148e820da5654ab076874ec7d3
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-09-14 08:51:00 -0500

    useMemo started

--- src/components/UseFetch/BlockQuote.jsx
+++ src/components/UseFetch/BlockQuote.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import styled from "styled-components";
 
 const Container = styled.blockquote`
@@ -17,15 +17,29 @@ const Container = styled.blockquote`
   }
 `;
 
-const BlockQuote = ({quote, author}) => {
-    return (
-        <Container>
-            <p>
-                <strong>{quote}</strong>
-            </p>
-            <footer>{author}</footer>
-        </Container>
-    )
+const BlockQuote = ({ quote, author }) => {
+
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    let { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({ width, height })
+  }, [quote])
+
+  return (
+    <Container>
+      <p ref={pRef}>
+        <strong>{quote}</strong>
+      </p>
+      <footer>{author}</footer>
+
+      <code>{JSON.stringify(boxSize)}</code>
+    </Container>
+  )
 }
 
 export default BlockQuote
